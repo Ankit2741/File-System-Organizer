5d62ba5919c4e2f4636fa3d7d6836a1c3bb93c13
Ankit Malhotra <ankitmalhotra1999@gmail.com>
2021-12-10 17:35:05 +0530

    Create FO.js

---
+++ FO.js
@@ -0,0 +1,30 @@
+
+
+//let input = process.argv[2]
+//console.log(input)
+const fs = require("fs");
+const path = require("path");
+const heplObj = require('./commands/help') 
+const treeObj = require('./commands/tree')
+const organizeObj = require('./commands/organize')
+
+let inputArr = process.argv.slice(2); 
+
+let command = inputArr[0]; 
+
+
+
+switch (command) {
+  case "tree":
+    treeObj.treeKey(inputArr[1]);
+    break;
+  case "organize":
+    organizeObj.organizeKey(inputArr[1]); 
+    break;
+  case "help":
+    heplObj.helpFnKey()
+    break;
+  default:
+    console.log("PLEASE ENTER A VALID COMMAND");
+    break;
+}
